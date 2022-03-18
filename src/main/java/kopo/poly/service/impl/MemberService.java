04ddbdeeea1eb6453df23e402e96941bde37b898
ohanmin <ohhanmin000@gmail.com>
2022-03-18 22:37:13 +0900

    third :  secure

--- src/main/java/kopo/poly/service/impl/MemberService.java
+++ src/main/java/kopo/poly/service/impl/MemberService.java
@@ -1,5 +1,20 @@
 package kopo.poly.service.impl;
 
-public class MemberService {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class MemberService implements UserDetailsService {
+
+    //사용자가 입력한 identifier을 통해 디비에 저장된 유저 객체를 가져와서 User
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
--- src/main/java/kopo/poly/util/PasswordEncoder.java
+++
@@ -1,6 +0,0 @@
-package kopo.poly.util;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class PasswordEncoder {
-}
